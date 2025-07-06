3369015a0ef776c0668b4bd71b1f4012dc715c6e
TarekGTK <115398923+TarekGTK@users.noreply.github.com>
2025-07-06 22:16:53 +0100

    implementation

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Component
+import { LayoutComponent } from './layouts/layout.component';
+import { AuthlayoutComponent } from './authlayout/authlayout.component';
+import {AdminGuard} from "./core/guards/admin.guard";
+import {LayoutComponentUser} from "./layout-user/layout.component";
+import { CondidatGuard } from './core/guards/condidat.guard';
+import { RhGuard } from './core/guards/rh.guard';
+import { RecruteurGuard } from './core/guards/recruteur.guard';
+
+
+const routes: Routes = [
+  { path: '', component: LayoutComponent, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [AdminGuard] },
+  { path: 'User', component: LayoutComponentUser, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [CondidatGuard]  },
+  { path: 'Company', component: LayoutComponentUser, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [RhGuard]  },
+  { path: 'Staff', component: LayoutComponentUser, loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule), canActivate: [RecruteurGuard]  },
+  { path: 'auth', component: AuthlayoutComponent, loadChildren: () => import('./account/account.module').then(m => m.AccountModule) },
+  { path: 'pages',component: AuthlayoutComponent, loadChildren: () => import('./extraspages/extraspages.module').then(m => m.ExtraspagesModule)/*,canActivate: [userGuard]*/},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top' })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/pages/extrapages/stage/integrationbi/integrationbi.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-integrationbi',
+  standalone: true,
+  imports: [],
+  templateUrl: './integrationbi.component.html',
+  styleUrl: './integrationbi.component.scss'
+})
+export class IntegrationbiComponent {
+
+}
